faf8053590372cbc114d405a12eb8e8e698204d1
Tobias Brunner <tobias@wuala.com>
2015-02-18 11:18:51 +0100

    merge config_system hash with default values

--- manifests/config.pp
+++ manifests/config.pp
@@ -2,9 +2,15 @@
 #
 # This class is called from knot for service config.
 #
-class knot::config inherits knot {
+class knot::config {
 
-  file { $config_file:
+  # merge two hashes:
+  # * the configuration hash from the user calling $knot::config_system
+  # * the hash containing the default values in $knot::params::config_system
+  # When there is a duplicate key, the key in the user specified hash "wins"
+  $config_system = merge($knot::params::config_system, $knot::config_system)
+
+  file { $knot::config_file:
     ensure  => file,
     content => template('knot/knot.conf.erb'),
   }
