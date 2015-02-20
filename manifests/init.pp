5c310f4d3e274e248ec101b42433827bcdf3f4c9
Tobias Brunner <tobias@wuala.com>
2015-02-20 11:13:32 +0100

    input validation and param rearrangement

--- manifests/init.pp
+++ manifests/init.pp
@@ -35,26 +35,48 @@ class knot (
   $main_config_file     = $::knot::params::main_config_file,
   $zones_config_file    = $::knot::params::zones_config_file,
   $system               = $::knot::params::system,
-  $zone_options         = undef,
   $log                  = $::knot::params::log,
   $interfaces           = $::knot::params::interfaces,
   $control              = $::knot::params::control,
   $keys                 = undef,
   $remotes              = undef,
   $groups               = undef,
-  $zone_storage         = $::knot::params::zone_storage,
   $dnssec_enable        = $::knot::params::dnssec_enable,
   $dnssec_keydir        = $::knot::params::dnssec_keydir,
+  $zone_storage         = $::knot::params::zone_storage,
   $zone_defaults        = undef,
+  $zone_options         = undef,
   $zones                = {},
 ) inherits ::knot::params {
 
-  # validate parameters here:
-  # validate_absolute_path, validate_bool, validate_string, validate_hash
-  # validate_array, ... (see stdlib docs)
+  # mandatory parameters
+  validate_re($package_ensure, '^installed|present|absent|purged|held|latest$')
+  validate_string($package_name)
+  validate_string($package_distcodename)
+  validate_bool($manage_package_repo)
+  validate_string($service_name)
+  validate_bool($service_enable)
+  validate_re($service_ensure, '^stopped|false|running|true$')
+  validate_bool($service_manage)
+  validate_string($service_user)
+  validate_string($service_group)
+  validate_string($main_config_file)
+  validate_string($zones_config_file)
+  validate_hash($system)
+  validate_hash($log)
+  validate_hash($interfaces)
+  validate_hash($control)
+  validate_string($zone_storage)
+  validate_bool($dnssec_enable)
+  validate_string($dnssec_keydir)
+  validate_hash($zones)
+
+  # optional parameters
   if $keys { validate_hash($keys) }
   if $remotes { validate_hash($remotes) }
   if $groups { validate_hash($groups) }
+  if $zone_options { validate_hash($zone_options) }
+  if $zone_defaults { validate_hash($zone_defaults) }
 
   class { '::knot::install': } ->
   class { '::knot::config': } ~>
